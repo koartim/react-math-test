69c6a62cd5d4d13b49d8d577ea2bd9d6ee290bcc
Tim Koar <koartimothy@gmail.com>
2020-04-08 14:33:06 -0400

    adding mathjs api

--- src/components/Multiplication.js
+++ src/components/Multiplication.js
@@ -1,10 +1,27 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export class Multiplication extends Component {
+export class Multiplication extends React.Component {
+
+  state = {
+    answer: ""
+  }
+
+  componentDidMount() {
+    fetch('http://api.mathjs.org/v4/?expr=2*(7-3)')
+      .then(rsp => rsp.json())
+      .then(data => {
+        this.setState({
+          answer: data.toString()
+        })
+      })
+  }
+
     render() {
+      const answer = this.state.answer
         return (
             <div>
                 <h1>Multiplication</h1>
+                  <p>{answer}</p>
             </div>
         )
     }
